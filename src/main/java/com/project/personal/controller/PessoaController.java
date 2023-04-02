89bc0bc2b3c74b7e25ac43c4daffb3c19a58066a
igorsalgado <salgado.igor.is@gmail.com>
2023-04-02 16:49:35 -0300

    Criando Requisição GET e Tela para remover de pesssoas

--- src/main/java/com/project/personal/controller/PessoaController.java
+++ src/main/java/com/project/personal/controller/PessoaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
+@SuppressWarnings("ALL")
 @Controller // Indica que a classe é um controlador
 public class PessoaController {
 
@@ -22,7 +23,7 @@ public class PessoaController {
         return modelAndView;
     }
 
-    @PostMapping("*/salvarpessoa") // Indica que o método será chamado quando a url for /salvarpessoa e ** ignora qualquer coisa que vier antes
+    @PostMapping("**/salvarpessoa") // Indica que o método será chamado quando a url for /salvarpessoa e ** ignora qualquer coisa que vier antes
     public ModelAndView salvar(Pessoa pessoa) {
         pessoaRepository.save(pessoa);
         Iterable<Pessoa> pessoasIt = pessoaRepository.findAll(); // Retorna todos os registros da tabela pessoa
@@ -45,6 +46,15 @@ public class PessoaController {
         Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa); // Retorna o registro da tabela pessoa que possui o id igual ao idpessoa
         modelAndView.addObject("pessoaobj", pessoa.get()); // Adiciona o objeto pessoa no atributo pessoaobj
         return modelAndView; // Injeta o objeto pessoa no formulário
+
+    }
+
+    @GetMapping("/removerpessoa/{idpessoa}")
+    public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {
+        pessoaRepository.deleteById(idpessoa); // Deleta o registro da tabela pessoa que possui o id igual ao idpessoa
+        modelAndView.addObject("pessoas",pessoaRepository.findAll());
+        modelAndView.addObject("pessoaobj", new Pessoa()); // Adiciona o objeto pessoa no atributo pessoaobj
+        return modelAndView; // Retorna a view como resposta
 
     }
 
